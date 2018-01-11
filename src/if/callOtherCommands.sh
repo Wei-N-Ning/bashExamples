3c99f04f52dd2b783bcdacefe2363c2e557a7875
Wei Ning <nwcgnw@gmail.com>
2018-01-11 19:56:05 +1300

    - add compound logic examples - add examples showing how to test the exit-status of previous statement/command

--- src/if/callOtherCommands.sh
+++ src/if/callOtherCommands.sh
@@ -7,8 +7,19 @@ fi
 
 
 # test the return code from a command
-touch /tmp/dd
+if ! (touch /tmp/dd) then
+    exit 1
+fi
 
+
+if ! (python -c "exit(0)") then
+    exit 1
+fi
+
+
+if (python -c "exit(1)") then
+    exit 1
+fi
 
 
 echo "pass"
--- src/if/logics.sh
+++ src/if/logics.sh
@@ -8,6 +8,11 @@ fi
 
 
 # logic-or
+if [ "1" = "!" ] || [ "2" = "2" ]; then
+    echo
+else
+    exit 1
+fi
 
 
 echo "pass"
