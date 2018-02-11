c109fb6641749410f4201e48fa4cfd2e75658910
Wei Ning <nwcgnw@gmail.com>
2018-02-12 09:32:37 +1300

    - add new sub dir: logging (showing how to handle logging and output redirection)

---
+++ src/logging/redirectOutput.sh
@@ -0,0 +1,16 @@
+
+# posts:
+# https://unix.stackexchange.com/questions/182537/write-python-stdout-to-file-immediately
+# https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file
+
+# bash version
+python -c "import time,sys;[(time.sleep(0.1), sys.stdout.write('XXAA\n')) for i in xrange(0xA)]" >& /tmp/log.txt
+cat /tmp/log.txt
+
+# change stdbuf behavior
+stdbuf -oL python -c "import time,sys;[(time.sleep(0.1), sys.stdout.write('XXAA\n')) for i in xrange(0xA)]" &> /tmp/log.txt
+
+# change -oL to -eL to modify stderr buffering behavior 
+
+# csh version (can't run it here)
+# .... >& /tmp/log.txt
