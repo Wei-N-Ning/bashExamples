735d108cb285ff1e92a3055d16a33435e711a252
Wei Ning <nwcgnw@gmail.com>
2018-02-12 20:04:04 +1300

    - add more cases in return value example (return by output-param; return by var-expansion)

--- src/functions/returnValue.sh
+++ src/functions/returnValue.sh
@@ -2,16 +2,64 @@
 # http://www.linuxjournal.com/content/return-values-bash-functions
 
 
-function setGlobalVarInFunction {
-    echo
+# $1: if not empty, "returns" a string; otherwise return 1
+function returnByVarExpansion {
+    if [ -z "$1" ]; then
+        return 1
+    else
+        echo "doom 1992"
+    fi
+}
+
+
+# $1: if not empty, "returns" a string via its output parameter
+# $2: an output parameter; its original value will be erased
+# The eval statement basically tells bash to interpret the line twice, 
+# the first interpretation above results in the string 
+# 
+# result='some value' 
+# 
+# which is then interpreted once more and ends up setting the caller's 
+# variable.
+function returnByOutputParam {
+    if [ -z "$1" ]; then
+        return 1
+    fi
+    if [ -z $2 ]; then
+        return 1
+    fi
+    eval $2="'doom 1992'"
 }
 
 
 
 function run {
-    setGlobalVarInFunction
+    if [ ! "$(returnByVarExpansion)" = "" ]; then
+        echo "failed"
+        exit 1
+    fi
     
-    echo
+    if [ ! "$(returnByVarExpansion a)" = "doom 1992" ]; then
+        echo "failed"
+        exit 1
+    fi
+    
+    local ret=""
+    
+    returnByOutputParam "a"
+    returnByOutputParam ret
+    returnByOutputParam "a" "__ret"
+    if [ ! "$ret" = "" ]; then
+        echo "failed"
+        exit 1
+    fi
+    
+    returnByOutputParam "a" ret 
+    if [ ! "$ret" = "doom 1992" ]; then
+        echo "failed"
+        exit 1
+    fi
+    
 }
 
 
