27621c3ea601b1612db863f361402d03a7228037
Wei Ning <wning@wetafx.co.nz>
2018-02-23 18:51:32 +1300

    - add more logging examples

--- src/logging/printing.sh
+++ src/logging/printing.sh
@@ -7,8 +7,21 @@ function callPrintf() {
 }
 
 
+# pro bash P12
+function printInColumns() {
+    printf "%8s %-15s:\n" 1a 2a 3a 4a 5a 6a
+}
+
+function printToVariable() {
+    local __output=""
+    printf -v __output "abc"
+    echo $__output
+}
+
 function run() {
     callPrintf
+    printInColumns
+    printToVariable
 }
 
 
