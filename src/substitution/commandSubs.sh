36ea90450ad872302a9f9686f4ff7c5152590c0e
Wei Ning <nwcgnw@gmail.com>
2018-02-27 23:17:08 +1300

    - add command and variable/parameter expansion demos

--- src/substitution/commandSubs.sh
+++ src/substitution/commandSubs.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# from pro bash
 # recommending: date=$( date ) to date=`date`
 
+# see: http://www.compciv.org/topics/bash/variables-and-substitution/
+# Command substitution allows the output of a command to replace 
+# the command itself. Bash performs the expansion by executing command 
+# and replacing the command substitution with the standard output of 
+# the command, with any trailing newlines deleted. Embedded newlines 
+# are not deleted, but they may be removed during word splitting.
 
+# newline character is no longer used as the splitter
+function commandExpansionUsingCustomIFS() {
+    local IFS=$'a'
+    assertStringEqual $'1\n2\n3\n4\n5\n6\n7\n8\n9\n10' $(seq 1 10)
+}
+
+# newliner character (and space, tab characters) is the splitter, 
+# therefore $(seq 1 10) expands to 1
+function commandExpansionUsingDefaultIFS() {
+    assertStringEqual "1" $(seq 1 10)
+}
+
+function assertStringEqual() {
+    if [ ! "$1" = "$2" ]; then
+        printf "failed!\n"
+        exit 1
+    fi
+}
+
+function run() {
+    commandExpansionUsingCustomIFS
+    commandExpansionUsingDefaultIFS
+}
+
+run
---
+++ src/substitution/variableSubs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+# see: https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
+function useCurlyBracketsToProtectVariable() {
+    local idd="..."
+    local iddqd="invincible"
+    assertStringEqual "${idd}qd" "...qd"
+    assertStringEqual "$iddqd" "invincible"
+}
+
+function stringLength() {
+    local str="iddqd"
+    assertStringEqual "${#str}" "5"
+}
+
+function findAndReplace() {
+    local idkfa="key.firearms.ammos"
+    assertStringEqual "${idkfa/firearms/swords}" "key.swords.ammos"
+    
+    # to replace all matches of pattern
+    assertStringEqual "${idkfa//./|}" "key|firearms|ammos"
+    
+    # exploit this technique to rename or remove files on the fly
+    filePath="/tmp/store.json"
+    touch "$filePath"
+    if ! (cp "${filePath}" "${filePath/.json/.bson}"); then
+        printf "failed!"
+        exit 1
+    fi
+}
+
+function assertStringEqual() {
+    if [ ! "$1" = "$2" ]; then
+        printf "failed!\n"
+        exit 1
+    fi
+}
+
+function run() {
+    useCurlyBracketsToProtectVariable
+}
+
+run
