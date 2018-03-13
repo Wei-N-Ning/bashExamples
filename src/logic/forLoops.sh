1726b3e1ab7aaad4c4543e06cb90007e91bbcec7
Wei Ning <wning@wetafx.co.nz>
2018-03-14 09:21:33 +1300

    - add apache server log samples;
    - update for loop example (grep v.s. a more advanced awk program)

--- src/logic/forLoops.sh
+++ src/logic/forLoops.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
+# showing two methods of finding the (missing) file resources that
+# had caused server faults and the number of faults caused by each 
+# resource
 
+function doubleGrep() {
+    for line in $( grep -i "file does not exist" ../_testdata/error_log | awk '{print $13}' | sort | uniq )
+    do
+        printf "missing file: %s, " $line
+        printf "num hit: %s\n" $( grep $line ../_testdata/error_log | wc -l  )
+    done
+}
+
+function useAwkArray() {
+    grep -i "file does not exist" ../_testdata/error_log |\
+    awk '
+{
+    arr[$13] += 1
+}
+END {
+    for (filePath in arr) {
+        printf "missing file: %s, num hit: %d\n", filePath, arr[filePath]
+    }
+}
+' | sort
+}
+
+function run() {
+    doubleGrep
+    printf  "***********\n"
+    useAwkArray
+}
+
+run
 
