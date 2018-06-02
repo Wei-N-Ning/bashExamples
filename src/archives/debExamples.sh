f8683494d88c15f96f782d0a523f327316a7b878
Wei Ning <nwcgnw@gmail.com>
2018-06-02 19:57:46 +1200

    - add link to source document for debExamples.sh

--- src/archives/debExamples.sh
+++ src/archives/debExamples.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash env
 
+# source:
+# https://linux-tips.com/t/how-to-extract-deb-package/169
+
+# side note: where to find such .deb files 
+# http://security.ubuntu.com/ubuntu/pool/main
+# example: libx/libxp
+
 debPath="../_testdata/libxp6_1.0.0-1_amd64.deb"
 
 setUp() {
