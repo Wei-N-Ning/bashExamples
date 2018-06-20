549f29836f8cdffd868c0a7b87e950944201ecdc
Wei Ning <wning@wetafx.co.nz>
2018-06-20 18:32:59 +1200

    - bash grep demo

---
+++ src/regexp/grep_.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# dig file paths from the contents in the directory recursively
+# -h: hide file name
+# rgrep -hPo '((/\S+)+/\w+\.ma)'
+
+# grep -Po '(/\w+)+/\w+\.koru' ${filePath} 
+
+
