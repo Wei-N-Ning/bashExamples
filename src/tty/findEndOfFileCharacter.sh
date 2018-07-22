cc7f6810c0da7c373fb7b296f2eee28bb0f5a42d
Wei Ning <nwcgnw@gmail.com>
2018-07-23 00:19:33 +1200

    - add interesting tty config example

---
+++ src/tty/findEndOfFileCharacter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# source:
+# linux socket programming by examples P179
+
+# don't use in a script
+# stty -a
+
+# to change your terminal end of file character to CTRL+D:
+# stty eof \^d
