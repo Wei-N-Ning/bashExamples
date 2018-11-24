b9d84008a9781449ba5c6e4b6bad02d9da631d7a
Wei Ning <nwcgnw@gmail.com>
2018-11-24 17:43:33 +1300

    - add more noted

--- src/archives/zipSelfExtracting.sh
+++ src/archives/zipSelfExtracting.sh
@@ -7,6 +7,13 @@
 # https://github.com/megastep/makeself
 # https://makeself.io/
 
+# NOTE:
+# however the tar-based self-extracting installer requires a step to pipe the binary blob to the tar program
+# https://www.linuxjournal.com/content/add-binary-payload-your-shell-scripts
+# https://www.linuxjournal.com/node/1005818
+# https://stackoverflow.com/questions/955460/how-do-linux-binary-installers-bin-sh-work
+# https://stackoverflow.com/questions/20758981/self-extracting-script-in-sh-shell
+
 TEMPDIR=/tmp/sut
 
 tearDown() {
