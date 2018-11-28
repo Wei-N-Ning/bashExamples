27e8c40442b8f6d80d4cf3c6fc2f9a847c0ede2a
Wei Ning <nwcgnw@gmail.com>
2018-11-28 11:59:49 +1100

    - update xargs call shell script example

--- src/xargs/callSubshell.sh
+++ src/xargs/callSubshell.sh
@@ -3,12 +3,19 @@
 # source
 # https://unix.stackexchange.com/questions/209249/piping-commands-after-a-piped-xargs
 
-# note that it invokes sh/bash to execute a complex
-# shell script
+# note that it invokes sh/bash to execute a more complex
+# shell script than a one-liner;
+# it is very useful when I need to pass the arguments to a
+# function-like execution unit;
+
+# see also numArgs.sh example that explains xargs -n
 
 main() {
     find . -type f |\
-    xargs -n 1 sh -c 'echo ////////$0; head -3 $0'
+    xargs -n 1 sh -c '
+echo "+ filename: "$0
+head -3 $0
+'
 }
 
 main
