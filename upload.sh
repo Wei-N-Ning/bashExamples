b36a311a7b3d031d1c8a9e1d7aa680bf5ff404d3
Wei.n <macgnw@gmail.com>
2018-12-21 15:20:41 +1100

    update script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-bash ./src/fileSystem/find/walkDirs.sh $( pwd )/src > ./README.md
+thisdir=$( dirname ${BASH_SOURCE[0]} )
+bash ${thisdir}/src/fileSystem/find/walkDirs.sh ${thisdir}/src > ./README.md
 git add .
 git commit -a -m "automatically update readme markdown"
 git push
