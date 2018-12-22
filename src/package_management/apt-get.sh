0815b4e11c972f4dc60f8901696c815ec9184aec
Wei.n <macgnw@gmail.com>
2018-12-22 15:05:43 +1100

    package management

---
+++ src/package_management/apt-get.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt_get_no_prompt() {
+    sudo apt-get --assume-yes install xxx
+}
+
---
+++ src/package_management/yum.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+yum_no_prompt() {
+    sudo yum install xxx -y
+}
+
+
