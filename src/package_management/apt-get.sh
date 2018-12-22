d4a173b6c03a76a471b252bdc2127905296f3363
Wei Ning <nwcgnw@gmail.com>
2018-12-22 15:58:54 +1100

    update package man

--- src/package_management/apt-get.sh
+++ src/package_management/apt-get.sh
@@ -4,6 +4,7 @@
 # https://askubuntu.com/questions/448358/automating-apt-get-install-with-assume-yes
 
 apt_get_no_prompt() {
-    sudo apt-get --assume-yes install xxx
+    sudo apt-get update
+    sudo apt-get --assume-yes install python-pip
 }
 
