52b9815938c4508b00f8dd909d5fe1ebd7b29391
Wei.n <macgnw@gmail.com>
2018-12-26 09:11:27 +1100

    complete file perm example

--- src/fileSystem/show_file_permission.sh
+++ src/fileSystem/show_file_permission.sh
@@ -9,6 +9,30 @@
 # stat -f "%A" sut
 # Linux:
 # stat -c "%a" sut
+
+# $1: filename
+# return: file permission in octal
 get_perm() {
-    :
+    local filename=${1:?"missing filename!"}
+    if [[ "$( uname )" == "Linux" ]]
+    then
+        stat -c "%a" ${filename}
+    else
+        stat -f "%A" ${filename}
+    fi
 }
+
+test_get_perm() {
+    get_perm /tmp
+    local f=/tmp/__readonly && touch ${f} && chmod 400 ${f} && \
+        get_perm ${f} && rm ${f}
+    get_perm /dev
+
+    # throws an error on non existing file 
+    ( get_perm /etc/_default )
+    
+    get_perm /
+}
+
+test_get_perm
+
