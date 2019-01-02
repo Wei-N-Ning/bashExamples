a05e0133affc805a6d0eb5f3789c40d8b52e716e
Wei.n <macgnw@gmail.com>
2019-01-03 08:43:35 +1100

    how to turn the result of ls and find (prefer find) into a bash array, updated

--- src/containers/string_as_array.sh
+++ src/containers/string_as_array.sh
@@ -1,12 +1,23 @@
 
 # find in canva infra codebase
 
-demo() {
+# turn the results of find and ls into a bash array
+ls_files_as_array() {
     local a=( $(ls /tmp) )
     for eac in "${a[@]}"
     do
-        echo ${eac}
+        echo -n "."
     done
+    echo "${#a[@]}"
 }
 
-demo
+find_files_as_array() {
+    local root=$(git rev-parse --show-toplevel)
+    local a=( $(find "${root}" -name "*.sh") )
+    echo "${#a[@]}"
+}
+
+ls_files_as_array
+find_files_as_array
+
+
