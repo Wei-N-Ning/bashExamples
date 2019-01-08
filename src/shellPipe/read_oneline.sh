1d311c76b3c65d0ae37edf308ff8f979a554c07e
Wei.n <macgnw@gmail.com>
2019-01-08 17:48:54 +1100

    shell pipe

--- src/shellPipe/read_oneline.sh
+++ src/shellPipe/read_oneline.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # see:
 # https://renenyffenegger.ch/notes/Linux/shell/bash/built-in/read
 
@@ -24,9 +25,18 @@ use_custom_IFS() {
     echo "name: ${name}"
 }
 
+# #  Waiting for the user to press any key
+#    -n 1: one character is enough
+#    -s  : suppress echoing the key that was pressed
+#    -p  : What should be prompted
+press_any_key_to_continue() {
+    read -n 1 -s -p "any key"
+}
+
 main() {
     use_default_IFS
     use_custom_IFS
+    press_any_key_to_continue
 }
 
 main
