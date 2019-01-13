fe09f2c0314a9d19e73e5aa472d3f9219a6e9893
Wei Ning <macgnw@gmail.com>
2019-01-13 11:42:01 +1100

    implement perl-like die()

--- src/logging/shell_logging.sh
+++ src/logging/shell_logging.sh
@@ -4,8 +4,15 @@ error() {
     echo "$(date '+[%H:%M:%S]') ERROR:" $@
 }
 
+DIE='eval error die at ${BASH_SOURCE[0]}:$LINENO'
+
 main() {
     error test failed
+}
+
+# implement a perl-like die "function"
+demo_die() {
+    ${DIE}
 }
 
 main
