7987e8a0c9117bbd39d95faa65eb721799f14b86
Wei Ning <macgnw@gmail.com>
2019-02-12 21:19:06 +1100

    scope

--- src/variables/scope.sh
+++ src/variables/scope.sh
@@ -3,6 +3,7 @@
 # motivation
 # to test the idea of splitting up GOD function
 
+# notice how the modification is in effect in the caller's scope
 export_something_impl() {
     aws_foo="asd"
     aws_bar="bsd"
@@ -16,4 +17,16 @@ export_something() {
     echo "after: ${aws_foo} ${aws_bar}"
 }
 
+# notice how the exported variable is seen by the perl process
+export_global() {
+    export AWS_FOO="${aws_foo}"
+    export AWS_BAR="${aws_bar}"
+}
+
 export_something
+export_global
+
+perl <<"EOF"
+defined($ENV{AWS_FOO}) || die "missing AWS_FOO";
+defined($ENV{AWS_BAR}) || die "missing AWS_BAR";
+EOF
