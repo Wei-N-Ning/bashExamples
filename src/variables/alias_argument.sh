48c3b5f5b30d342250b8fe42bbbb42f121c078f3
Wei Ning <macgnw@gmail.com>
2019-02-16 13:34:05 +1100

    how not to use shell aliases

---
+++ src/variables/alias_argument.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# motivation:
+# can alias use "argument" ??
+# NO!!!
+
+# solution: define a bash function
+# but be CAREFUL! if the function calls exit() it will crash the 
+# calling shell!
