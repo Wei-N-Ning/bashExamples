487bb626149250075811a3f149e0fba7c527bb20
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-04 12:56:52 +1000

    wip

--- src/strings_/cutColumns.sh
+++ src/strings_/cutColumns.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # example of inputs:
 # wein     console  Dec 28 06:59
 # wein     ttys000  Jan  7 17:27
@@ -14,11 +16,21 @@ function cutColumns() {
 
 extract_columns() {
     who | perl -lane "print @F[0]"
+
 }
 
 function run() {
     echo "//// bash cut" && cutColumns
     echo "//// perl field" && extract_columns
+
+    local name
+    local value
+    while read -r name value; do
+        echo "${name} + ${value}"
+    done < <(who | perl -anl -F'/\s+/' -w -E 'say "$F[1] $F[2]"')
+
+    read -r name value < <(who | perl -anl -F'/\s+/' -w -E 'say "$F[0] $F[1]"')
+    echo "${name} .. ${value}" 
 }
 
 run
